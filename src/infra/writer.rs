4a12ded472c8e69b62ca4608989263bae1b21157
r1cm3d <r1cm3d@protonmail.com>
2023-01-25 11:02:56 -0300

    writer: Create dir if it does not exist
    Signed-off-by: r1cm3d <r1cm3d@protonmail.com>

--- src/infra/writer.rs
+++ src/infra/writer.rs
@@ -1,6 +1,8 @@
 use crate::types::Writer;
 use std::io::Write;
 use std::fs::File;
+use std::fs;
+use std::path::PathBuf;
 use log::info;
 
 pub struct Fs {}
@@ -8,8 +10,15 @@ pub struct Fs {}
 impl Writer for Fs {
 
     fn write(&self, file_name: &str, content: &str) -> Option<Box<dyn std::error::Error>> {
-        info!("Writing File ({}).", file_name);
+        info!("Extracting path of ({}).", file_name);
+        let path = PathBuf::from(file_name);
+        let dir = path.parent()?;
 
+        info!("Creating directory ({}).", dir.to_str()?);
+        fs::create_dir_all(dir).ok()?;
+
+
+        info!("Writing File ({}).", file_name);
         let mut file = File::create(file_name).ok()?;
         file.write_all(content.as_bytes()).ok()?;
 
